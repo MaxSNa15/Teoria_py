5456af4547368fc80375ab8e358657e0f3270276
Max Nava <max.s015@outlook.com>
2024-02-19 15:44:32 -0600

    Añadir archivo .gitignore y realizar cambios en el grafo

--- Resources/02_Conjuntos/grafo.py
+++ Resources/02_Conjuntos/grafo.py
@@ -7,7 +7,7 @@
 # G = (V, E) donde V es el conjunto de vértices y E es el conjunto de aristas
 
 ver = (
-    'Virgo', 'Coruña', 'Valladolid', 'Madrid', 'Bilbao', 'Oviedo', 'Zaragoza', 
+    'Virgo', 'Coruña', 'Valladolid', 'Madrid', 'Bilbao', 'Oviedo', 'Zaragoza',
     'Barcelona', 'Gerona', 'Badajoz', 'Valencia', 'Jaen', 'Albacete', 'Murcia',
     'Cadiz', 'Sevilla', 'Granada'
 )
@@ -19,7 +19,7 @@ ari = {
     ('Bilbao', 'Oviedo', 304), ('Bilbao', 'Zaragoza', 324), ('Bilbao', 'Madrid', 395),
     ('Madrid', 'Zaragoza', 325), ('Madrid', 'Badajoz', 403), ('Madrid', 'Jaen', 335), ('Madrid', 'Albacete', 251),
     ('Albacete', 'Murcia', 150), ('Albacete', 'Valencia', 191),
-    ('Valencia', 'Murcia', 241), ('Valencia', 'Barcelona', 349), 
+    ('Valencia', 'Murcia', 241), ('Valencia', 'Barcelona', 349),
     ('Barcelona', 'Gerona', 100), ('Barcelona', 'Zaragoza', 296),
     ('Granada', 'Jaen', 99), ('Granada', 'Sevilla', 256), ('Granada', 'Murcia', 278),
     ('Sevilla', 'Cadiz', 125), ('Sevilla', 'Jaen', 242)
@@ -32,7 +32,7 @@ def construirGrafo(grafo):
     g = grafo()
     for v in ver:
         g.addNodo(Nodo(v))
-    
+
     for (origen, destino, peso) in ari:
         g.addArista(Arista(g.getNodo(origen), g.getNodo(destino), peso))
 
@@ -86,44 +86,74 @@ def BFS2(grafo, inicio, fin):
                 nuevo_path = actual_path + [sn]
                 queue.append(nuevo_path)
 
+# Definimos nuevamente la función DFS para la búsqueda en profundidad
+def DFS(grafo, inicio, visitados=None):
+    if visitados is None:
+        visitados = set()
+    visitados.add(inicio)
+
+    print(inicio.getNombre())
+
+    for vecino in grafo.getVecinos(inicio):
+        if vecino not in visitados:
+            DFS(grafo, vecino, visitados)
+
+    return visitados
+
+# Re-construimos el grafo y ejecutamos DFS
+grafo = Undirected_Grafo()
+for v in ver:
+    grafo.addNodo(Nodo(v))
+
+for (origen, destino, peso) in ari:
+    origen_nodo = grafo.getNodo(origen)
+    destino_nodo = grafo.getNodo(destino)
+    if origen_nodo and destino_nodo:
+        grafo.addArista(Arista(origen_nodo, destino_nodo, peso))
+
+# Realizamos la búsqueda en profundidad (DFS)
+inicio = grafo.getNodo('Madrid')
+visitados = DFS(grafo, inicio)
+
+
 
 ########################################################################################
-print('=======================================================================')
-G1 = construirGrafo(Undirected_Grafo)
-print(G1)
-print('=======================================================================')
+# print('=======================================================================')
+# G1 = construirGrafo(Undirected_Grafo)
+# print(G1)
+# print('=======================================================================')
 
-path = DFSPath1(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'), [])
+# path = DFSPath1(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'), [])
 
-for i in path:
-    print(f'{i.getNombre()}', end=' ')
+# for i in path:
+#     print(f'{i.getNombre()}', end=' ')
 
-print()
-print('=======================================================================')
+# print()
+# print('=======================================================================')
 
-path2 = DFSPath2(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'), [], None)
+# path2 = DFSPath2(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'), [], None)
 
-for i in path2:
-    print(f'{i.getNombre()}', end=' ')
+# for i in path2:
+#     print(f'{i.getNombre()}', end=' ')
 
-print()
-print('=======================================================================')
+# print()
+# print('=======================================================================')
 
-path3 = BFS1(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'))
+# path3 = BFS1(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'))
 
-for i in path3:
-    print(f'{i.getNombre()}', end='-> ')
+# for i in path3:
+#     print(f'{i.getNombre()}', end='-> ')
 
-print()
-print('=======================================================================')
+# print()
+# print('=======================================================================')
 
-path4 = BFS2(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'))
+# path4 = BFS2(G1, G1.getNodo('Virgo'), G1.getNodo('Sevilla'))
 
-for i in path4:
-    print(f'{i.getNombre()}', end='-> ')
+# for i in path4:
+#     print(f'{i.getNombre()}', end='-> ')
 
-print()
-print('=======================================================================')
+# print()
+# print('=======================================================================')
 
 
 
